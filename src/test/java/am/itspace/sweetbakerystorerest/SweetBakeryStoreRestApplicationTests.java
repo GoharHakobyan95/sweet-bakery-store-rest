63ae56a355c46f5680d3371cd5ccf629f62a57cd
Gohar Hakobyan <hak.goh95@gmail.com>
2022-12-11 01:57:46 +0400

    first commit

---
+++ src/test/java/am/itspace/sweetbakerystorerest/SweetBakeryStoreRestApplicationTests.java
@@ -0,0 +1,13 @@
+package am.itspace.sweetbakerystorerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetBakeryStoreRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
